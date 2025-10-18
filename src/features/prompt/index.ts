b1c25826b09d3aea95002b67206c4883d74968ab
harukaxq <h.tamura@cibo.design>
2025-10-19 08:45:11 +0900

    feat: add GitHub Action to auto-generate prompts from templates

---
+++ src/features/prompt/index.ts
@@ -0,0 +1,9 @@
+import { getBugCheckPrompt } from "./query/get-bug-check"
+import { getCodeStyleReviewPrompt } from "./query/get-code-style"
+import { getSddPrompt } from "./query/get-sdd"
+
+export const promptGenerators: Record<string, () => Promise<string>> = {
+  "bug-check.md": getBugCheckPrompt,
+  "code-style-review.md": getCodeStyleReviewPrompt,
+  "sdd.md": getSddPrompt,
+}
